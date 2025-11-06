535014474534f63a0f78db93213d21f937d19cba
Niklas Niggemann <niklas.niggemann@codecentric.de>
2025-11-06 10:37:16 +0100

    reworked timer

--- duckdb_sqlite_comparison/main.py
+++ duckdb_sqlite_comparison/main.py
@@ -1,10 +1,21 @@
 import os
 import duckdb
-import timer
+import time
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 shared_resources = os.path.join(current_dir, '..', 'shared_resources')
 shared_resources = os.path.normpath(shared_resources)
 
-duckdb.sql(f"SELECT * FROM read_csv_auto('{shared_resources}/netflix_titles.csv')").show()
-duckdb.sql(f"SELECT count(*), type FROM read_csv_auto('{shared_resources}/netflix_titles.csv') GROUP BY type").show()
+class Timer:
+    def __enter__(self):
+        self._enter_time = time.time()
+
+    def __exit__(self, *exc_args):
+        self._exit_time = time.time()
+        print(f"{self._exit_time - self._enter_time:.2f} seconds elapsed")
+
+with Timer():
+    duckdb.sql(f"SELECT * FROM read_csv_auto('{shared_resources}/netflix_titles.csv')").show()
+
+with Timer():
+    duckdb.sql(f"SELECT count(*), type FROM read_csv_auto('{shared_resources}/netflix_titles.csv') GROUP BY type").show()
