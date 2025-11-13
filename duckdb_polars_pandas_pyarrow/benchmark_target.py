c111edbb076fba9e4c1917234f8eab05fa3a9536
Niklas Niggemann <niklas.niggemann@codecentric.de>
2025-11-13 14:14:26 +0100

    added comments

--- duckdb_polars_pandas_pyarrow/benchmark_target.py
+++ duckdb_polars_pandas_pyarrow/benchmark_target.py
@@ -1,17 +1,21 @@
-import time
-import psutil
-import os
+import psutil, time, os
 import duckdb_basics, polars_basics, pandas_basics
 
 def get_memory_usage_mb() -> float:
-    """Return current process memory usage in MB."""
+    # os.getpid() returns the process-ID of the current process
+    # psutil.process represents an OS process
     process = psutil.Process(os.getpid())
+    # Return current process memory usage in MB
+    # memory_info() returns a tuple with variable fields depending on the platform
+    # rss is the Resident Set Size and is the non-swapped physical memory a process has used
+    # the rss is returned in bytes; 1 megabyte is equal to 1024^2 bytes
     return process.memory_info().rss / (1024 * 1024)
 
 def main():
     start = time.perf_counter()
     mem_before = get_memory_usage_mb()
-    pandas_basics.purchases_per_event_by_category()
+    # change function for individual benchmarks
+    duckdb_basics.purchases_and_count()
     mem_after = get_memory_usage_mb()
     end = time.perf_counter()
     print(f"Memory = {mem_after - mem_before:.2f} MB")
--- duckdb_polars_pandas_pyarrow/main.py
+++ duckdb_polars_pandas_pyarrow/main.py
@@ -1,5 +1,4 @@
-import benchmark_individual, benchmark_runner, benchmark_target
-import duckdb_basics, polars_basics, pandas_basics
+import duckdb_basics
+import psutil
 
-# benchmark_individual.run_benchmark(duckdb_basics.purchases_per_event_by_category)
-
+print(psutil.virtual_memory())
